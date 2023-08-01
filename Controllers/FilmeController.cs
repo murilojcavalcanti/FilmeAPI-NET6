103d95a1435bc99347444c0d58bdcfe5fa18e0de
murilojcavalcanti <josemurilo7@gmail.com>
2023-08-01 18:39:24 -0300

    First Commit

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmeAPI_NET6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeAPI_NET6.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new();
+    [HttpPost]
+    public void AddFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmeAPI_NET6.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public string Lacamento { get; set;}
+    public int Duracao { get; set; }
+
+}
